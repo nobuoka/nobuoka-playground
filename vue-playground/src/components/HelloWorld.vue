1d4e15de9262176133ee28b2df40733f4f599b1f
Nobuoka Yuya <yuya.nobuoka@gmail.com>
2022-07-03 09:30:35 +0900

    Vue.js のコンポーネントを作成

--- vue-playground/src/components/HelloWorld.vue
+++ vue-playground/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <SampleComponent msg="SampleComponent を使用します"></SampleComponent>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,9 +34,11 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import SampleComponent from './SampleComponent.vue';
 
 export default defineComponent({
   name: 'HelloWorld',
+  components: { SampleComponent },
   props: {
     msg: String,
   },
---
+++ vue-playground/src/components/SampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="sample-component">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SampleComponent',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+</style>
